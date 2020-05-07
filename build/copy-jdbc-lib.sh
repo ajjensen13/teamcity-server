bdbce8fee1e2d6b3f4fd0141d738289845de50d9
A. Jensen <jensen.aaro@gmail.com>
2020-05-06 20:21:11 -0500

    create

---
+++ build/copy-jdbc-lib.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p /data/teamcity_server/datadir/lib/jdbc
+cp /usr/local/lib/postgresql-42.2.12.jar /data/teamcity_server/datadir/lib/jdbc
--- cmd/program/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/ajjensen13/template/internal/program"
-
-func main() {
-	program.Execute()
-}
